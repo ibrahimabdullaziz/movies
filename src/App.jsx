ca0da25b87f305a2ad251d9a4dd1b03f4bf83d37
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-02-03 10:42:09 +0200

    feat: add movies page and enhance UI with IMDb gold theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import { useTrendingMovies } from "./hooks/useMovies";
+import TrendingMoviesPage from "./pages/movies";
 
 function App() {
-  const { data: movies, isLoading, isError, error } = useTrendingMovies();
-
-  if (isLoading)
-    return <div className="p-10 text-white">Loading movies...</div>;
-  if (isError)
-    return <div className="p-10 text-red-500">Error: {error.message}</div>;
-
-  return (
-    <div className="min-h-screen bg-slate-950 text-white p-8">
-      <h1 className="text-3xl font-bold mb-6">Trending Movies</h1>
-      <ul className="list-disc ml-6">
-        {movies?.map((movie) => (
-          <li key={movie.id} className="mb-2 text-lg">
-            {movie.title} - ⭐ {movie.vote_average}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <TrendingMoviesPage />;
 }
 
 export default App;
